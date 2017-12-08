a6e595d08244ab924a040e2fe9a25581c3f4d3c3
Abhishek Sharma <hello.abhishek@outlook.com>
2017-12-09 04:08:18 +0530

    frontend commit

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ text.set({
 canvas.add(text);
 */
 
-var canvas = document.getElementById('c'),
+
+//sets the image in canvas
+/*var canvas = document.getElementById('c'),
     context = canvas.getContext('2d');
 
 make_base();
@@ -63,44 +65,36 @@ function make_base() {
     base_image.src = 'test.jpg';
     let width = base_image.naturalWidth;
     let height = base_image.naturalHeight;
-    let ratio = width/height;
-    context.fillStyle='white';
-    context.fillRect(0,0,400,400);
+    let ratio = width / height;
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, 400, 400);
     base_image.onload = function () {
-        context.drawImage(base_image, (400-400*ratio)/2,0, 400*ratio, 400);
+        context.drawImage(base_image, (400 - 400 * ratio) / 2, 0, 400 * ratio, 400);
     }
-}
+}*/
+
+var canvas = new fabric.Canvas('c');
+
+//set background color
+var bgcolor = new fabric.Rect({
+    top: 0,
+    left: 0,
+    fill: '#fff',
+    height: 400,
+    width: 400
+})
+canvas.add(bgcolor);
 
 
-
-
-
-$(function() {
-  $("#fileToUpload").on('change', function() {
-    // Display image on the page for viewing
-    readURL(this, "preview");
-
-  });
+//add the main image
+var background = document.getElementById('mainbg');
+let scale = 400 / background.width;
+var bgInstance = new fabric.Image(background, {
+    left: 0,
+    top: 0
 });
-
-function readURL(input, tar) {
-  if (input.files && input.files[0]) { // got sth
-
-    // Clear image container
-    $("#" + tar).removeAttr('src');
-
-    $.each(input.files, function(index, ff) // loop each image 
-      {
-
-        var reader = new FileReader();
-
-        // Put image in created image tags
-        reader.onload = function(e) {
-          $('#' + tar).attr('src', e.target.result);
-        }
-
-        reader.readAsDataURL(ff);
-
-      });
-  }
-}
+bgInstance.set({
+    scaleX: scale,
+    scaleY: scale
+});
+canvas.add(bgInstance);
