0ecb9a20ae818d6292cf50010a16cd9d75c2d0be
Aditya Dewan <aditya.dewan@collabora.co.uk>
2017-12-09 15:45:39 +0530

    display changed image on sellers website

--- ext/bg.js
+++ ext/bg.js
@@ -15,32 +15,38 @@ chrome.contextMenus.onClicked.addListener(function(item, tab) {
 			focused: true
 			// state: 'maximized'
 		}, function(win) {
-			// chrome.extension.getBackgroundPage().console.log(win, item.srcUrl);
-			// console.log(win, item.srcUrl);
 
-			// chrome.tabs.query(
-			// 	{active: true}, 
-			// 	function(tabs){ 
-			// 		chrome.tabs.sendMessage(
-			// 			win.id, 
-			// 			{
-			// 				action: "setURL",
-			// 				info: item
-			// 			}, 
-			// 			function(response) { 
-			// 				//some
-			// 				// chrome.windows.create({
-			// 				// 	// Just use the full URL if you need to open an external page
-			// 				// 	url: chrome.runtime.getURL('app/index.html'),
-			// 				// 	type: "popup",
-			// 				// 	focused: true
-			// 				// }, function(win) {
-			// 				// // ok
-			// 				// });
-			// 			}
-			// 		); 
-			// 	}
-			// );
+			chrome.tabs.onRemoved.addListener( function(tabId, removeInfo) {
+
+
+
+				chrome.extension.getBackgroundPage().console.log(win, item.srcUrl);
+				console.log(win, item.srcUrl);
+				chrome.tabs.query(
+					{active: true}, 
+					function(tabs){ 
+						chrome.tabs.sendMessage(
+							tab.id, 
+							{
+								action: "setImage",
+								info: win
+							}, 
+							function(response) { 
+								//some
+								// chrome.windows.create({
+								// 	// Just use the full URL if you need to open an external page
+								// 	url: chrome.runtime.getURL('app/index.html'),
+								// 	type: "popup",
+								// 	focused: true
+								// }, function(win) {
+								// // ok
+								// });
+							}
+						); 
+					}
+				);
+			});
+
 		}
 	);
 
