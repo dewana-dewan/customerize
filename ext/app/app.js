13831b9712ed9a5403c47479b36e335defc210fb
Abhishek Sharma <hello.abhishek@outlook.com>
2017-12-09 09:55:06 +0530

    fixed color issue

--- ext/app/app.js
+++ ext/app/app.js
@@ -1,78 +1,6 @@
-/*align canvas*/
 var textColor = '#fff';
 var fontName = 'Roboto';
-/*window.onload = window.onresize = function() {
-    let canvas = document.getElementById('c');
-    canvas.width = 400;
-    canvas.height = 400;
-}*/
 
-//var canvas = new fabric.Canvas('c');
-
-
-/*var rect2 = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: 'blue',
-    width: 60,
-    height: 60,
-    angle: 45,
-    selectable: true
-});*/
-
-
-/*canvas.add(rect2);*/
-//canvas.add(rect);
-
-/*var boom = function () {
-    rect.set({
-        fill: 'blue',
-        selectable: true
-    });
-    canvas.renderAll();
-}*/
-
-
-
-
-/*var imgElement = document.getElementById('blah');
-var imgInstance = new fabric.Image(imgElement, {
-  left: 100,
-  top: 100,
-  selectable: true
-});
-canvas.add(imgInstance);
-*/
-
-
-/*var text = new fabric.Text('hello world', { left: 100, top: 100});
-
-text.set({
-    fontFamily: 'Roboto'
-})
-
-canvas.add(text);
-*/
-
-
-//sets the image in canvas
-/*var canvas = document.getElementById('c'),
-    context = canvas.getContext('2d');
-
-make_base();
-
-function make_base() {
-    base_image = new Image();
-    base_image.src = 'test.jpg';
-    let width = base_image.naturalWidth;
-    let height = base_image.naturalHeight;
-    let ratio = width / height;
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, 400, 400);
-    base_image.onload = function () {
-        context.drawImage(base_image, (400 - 400 * ratio) / 2, 0, 400 * ratio, 400);
-    }
-}*/
 var URL = localStorage.getItem('url');
 $('#mainbg').attr('src', URL);
 
@@ -86,13 +14,13 @@ var bgcolor = new fabric.Rect({
     height: 400,
     width: 400,
     selectable: false
-})
+});
 canvas.add(bgcolor);
 
 
 //add the main image
 var background = document.getElementById('mainbg');
-let scale = 400 / background.width;
+var scale = 400 / background.width;
 var bgInstance = new fabric.Image(background, {
     left: 0,
     top: 0,
@@ -126,13 +54,21 @@ canvas.add(bgInstance);
 
 //add text
 var setColor = function (x) {
+    console.log('setColor called');
     textColor = '#' + x;
-    console.log(textColor);
+    console.log('Color set to '+ textColor);
+};
+
+var colors = document.getElementsByClassName('selector');
+
+for(i=0;i<colors.length;i++){
+    let temp = '#' + colors[i].id;
+    colors[i].addEventListener('click',function(){
+        console.log('New color chosen');
+        textColor = temp;
+    })
 }
-
-
 var addText = function () {
-    console.log('calll meeee');
 
     let textValue = document.getElementById('textvalue').value;
     if (textValue === "") {
@@ -152,3 +88,5 @@ var addText = function () {
 
     canvas.add(mytext);
 }
+
+document.getElementById('setText').addEventListener('click',addText);
